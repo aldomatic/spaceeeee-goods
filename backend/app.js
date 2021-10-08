daea39837a6d044c8f4e82bf2d7b68e90e121180
Aldo Lugo <aldo@aldomatic.com>
2021-10-08 11:41:32 -0500

    updated logic

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
-const path = require('path'), fs = require('fs'), readline = require('readline');
-const { formatDate } = require('./helper.js');
+// const path = require('path'), fs = require('fs'), readline = require('readline');
+import path from 'path';
+import fs from 'fs';
+import readline from 'readline';
+import { formatDate, downloadImagesFromApi } from './helper.js';
 
 const regex = new RegExp(/(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)(?:-| )(\d{1,2})(?:-|,)(?: |)(\d{4})|(\d{2})\/(\d{2})\/(\d{2})/g);
 const datesArray = [], apiURl = "https://api.nasa.gov/planetary/apod?api_key=iagHhDKxsJJZ78haOJ7uaqvF2hiqY0azAEqSL0d2"
@@ -14,12 +17,16 @@ const parseFile = (pathToFile) => {
   file.on('line', (line) => {
     let result = line.match(regex);
     if(result){
-      datesArray.push(formatDate(result[0]));
+      //datesArray.push(formatDate(result[0]));
+      const earthDate = formatDate(result[0]);
+      downloadImagesFromApi(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${earthDate}}&api_key=iagHhDKxsJJZ78haOJ7uaqvF2hiqY0azAEqSL0d2`, earthDate, () => {
+        console.log('Done Downloading Images!')
+      })
     }
   }).on('close', () => {
-    console.log(datesArray);
+    console.log('closed')
   });
 }
 
-// Fetch images by dates 
+// Kick things off.. fetch images by dates 
 parseFile(path.resolve("dates.txt"));
--- backend/helper.js
+++ backend/helper.js
@@ -1,5 +1,9 @@
+import fs from 'fs';
+import fetch from 'node-fetch';
+import path from 'path';
+
 // Helper function to format dates as YYYY-MM-DD
-module.exports.formatDate = (date) => {
+export const formatDate = (date) => {
     var d = new Date(date),
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
@@ -11,4 +15,28 @@ module.exports.formatDate = (date) => {
         day = '0' + day;
 
     return [year, month, day].join('-');
+}
+
+const createDir = (folderName) =>{
+    fs.mkdirSync(`./images/${folderName}`, { recursive: true })
+}
+
+const downloadImageLocally = (remoteUrl, folder) =>{
+    console.log(path.basename(remoteUrl), folder);
+}
+
+// Here we call the nasa api and download the images locally
+export const downloadImagesFromApi = async (url, earth_date, callback) => {
+    // create directory if not created
+    createDir(earth_date);
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(data && data.hasOwnProperty('photos')){
+        data.photos.forEach((photo) => {
+           downloadImageLocally(photo.img_src, `./images/${earth_date}`);
+        });
+    }
+
 }
