ebcb9bda4bf32e58d3f873a479a92a27352eded7
Aldo Lugo <aldo@aldomatic.com>
2021-10-08 10:36:42 -0500

    initial commit with some code for once

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,25 @@
-const fs = require('fs');
-const readline = require('readline');
+const path = require('path'), fs = require('fs'), readline = require('readline');
+const { formatDate } = require('./helper.js');
+
 const regex = new RegExp(/(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)(?:-| )(\d{1,2})(?:-|,)(?: |)(\d{4})|(\d{2})\/(\d{2})\/(\d{2})/g);
-const datesArray = []; 
+const datesArray = [], apiURl = "https://api.nasa.gov/planetary/apod?api_key=iagHhDKxsJJZ78haOJ7uaqvF2hiqY0azAEqSL0d2"
 
-// Helper function to format dates as YYYY-MM-DD
-const formatDate = (date) => {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
-
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
-
-    return [year, month, day].join('-');
-}
-
-// Here we read the file line by line and run the regex to find dates.
-const file = readline.createInterface({
-    input: fs.createReadStream('dates.txt'),
+const parseFile = (pathToFile) => {
+  // Here we read the file line by line and run the regex to find dates.
+  const file = readline.createInterface({
+    input: fs.createReadStream(pathToFile),
     output: process.stdout,
     terminal: false
-});
-file.on('line', (line) => {
-  let result = line.match(regex);
-  if(result){
-    datesArray.push(formatDate(result[0]));
-  }
-}).on('close', () => {
-  console.log(datesArray);
-});
+  });
+  file.on('line', (line) => {
+    let result = line.match(regex);
+    if(result){
+      datesArray.push(formatDate(result[0]));
+    }
+  }).on('close', () => {
+    console.log(datesArray);
+  });
+}
+
+// Fetch images by dates 
+parseFile(path.resolve("dates.txt"));
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Spaceeee Goods
+
+## Install
+
+## Usage
